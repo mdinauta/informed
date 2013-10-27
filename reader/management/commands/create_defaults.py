6782789e5374c419a85670b0f6d639d3be6308bf
Matt DiNauta <mattdinauta@Matts-MacBook-Pro.local>
2013-10-27 15:27:47 -0700

    added new feeds to demo user

--- reader/management/commands/create_defaults.py
+++ reader/management/commands/create_defaults.py
@@ -10,20 +10,27 @@ class Command(BaseCommand):
 	def handle(self, *args, **kwargs):
 
 		default_user = User.objects.get(username='demo')
-		feed_dicts = [{'topic': 'technology', 'feed_url': 'http://feeds.feedburner.com/TechCrunch/'},
+		feed_dicts = [
+					{'topic': 'technology', 'feed_url': 'http://feeds.feedburner.com/TechCrunch/'},
 					{'topic': 'technology', 'feed_url': 'http://feeds.feedburner.com/hacker-news-feed-50?format=xml'},
 					{'topic': 'pop culture', 'feed_url': 'http://feeds.gawker.com/gawker/full'},
 					{'topic': 'news', 'feed_url': 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml'},
 					{'topic': 'programming', 'feed_url': 'http://www.reddit.com/r/django/.rss'},
 					{'topic': 'programming', 'feed_url': 'http://www.reddit.com/r/programming/.rss'},
-					{'topic': 'pop culture', 'feed_url': 'http://feeds.theonion.com/avclub/music/'},
+					{'topic': 'music', 'feed_url': 'http://feeds.theonion.com/avclub/music/'},
 					{'topic': 'pop culture', 'feed_url': 'http://feeds.theonion.com/avclub/newswire/'},
-					{'topic': 'pop culture', 'feed_url': 'http://www.rottentomatoes.com/syndication/rss/in_theaters.xml'},
-					{'topic': 'pop culture', 'feed_url': 'http://www.rottentomatoes.com/syndication/rss/in_theaters.xml'},
+					{'topic': 'movies', 'feed_url': 'http://www.rottentomatoes.com/syndication/rss/in_theaters.xml'},
 					{'topic': 'programming', 'feed_url': 'http://feeds.feedburner.com/nettuts'},
-					{'topic': 'programming', 'feed_url': 'http://www.reddit.com/r/python/.rss'}
+					{'topic': 'programming', 'feed_url': 'http://www.reddit.com/r/python/.rss'},
+					{'topic': 'statistics', 'feed_url': 'http://feeds.feedburner.com/SimplyStatistics'},
+					{'topic': 'statistics', 'feed_url': 'http://mathbabe.org/feed/'},
+					{'topic': 'music', 'feed_url': 'http://www.okayplayer.com/news/feed/'},
+					{'topic': 'statistics', 'feed_url': 'http://blog.priceonomics.com/'},	
+					{'topic': 'statistics', 'feed_url': 'http://www.dataists.com/feed/'},
+					{'topic': 'statistics', 'feed_url': 'http://flowingdata.com/feed/'},
+					{'topic': 'san francisco', 'feed_url': 'http://smashingreader.com/The_Bold_Italic_s28463'},
+					{'topic': 'san francisco', 'feed_url': 'http://www.reddit.com/r/sanfrancisco/.rss'},
 					]
-
 		for a in feed_dicts:
 			already_exists = feed.objects.filter(topic=a['topic'], feed_url=a['feed_url'])
 			if len(already_exists) == 0:
